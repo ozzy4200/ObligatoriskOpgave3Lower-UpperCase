d820944fd93b13d752a271e91bdf082a9da8f1f7
Osman Bajric <osman_bih@hotmail.com>
2020-03-10 10:36:55 +0100

    Færdige opg3

--- src/js/index.ts
+++ src/js/index.ts
@@ -10,3 +10,27 @@ let user: Person = { firstName: "John", lastName: "Doe" };
 
 let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
 element.innerHTML = greeter(user);
+
+
+let buttonElement: HTMLButtonElement = <HTMLButtonElement>document.getElementById("Doit")
+buttonElement.addEventListener("click", textChanger);
+
+function textChanger(): void {
+let inPutElement: HTMLInputElement = <HTMLInputElement>document.getElementById("inputText");
+let inputString: string = inPutElement.value;
+
+
+let selectElement: HTMLSelectElement = <HTMLSelectElement>document.getElementById("Selector");
+let Selector: string = selectElement.value;
+
+let resultElement: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("output");
+
+if(Selector == "Uppercase"){
+    let result = inputString.toUpperCase();
+    resultElement.innerHTML = result;
+}
+else if(Selector == "Lowercase"){
+    let result = inputString.toLowerCase();
+resultElement.innerHTML = result;
+}
+}
